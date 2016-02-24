69267d24141fd4c694e42f1a831ffa9c8e5e680f
brayanrodbajo <bryan951112@gmail.com>
2016-02-23 21:36:22 -0500

    genera matriz en archivo arff

--- leer.py
+++ leer.py
@@ -7,7 +7,6 @@ def leer (directorio) :
 def matriz_ocurrencias (documentos, palabras) :
 	matriz = []
 	for doc in documentos: 
-		print (doc)
 		archivo = open(directorio + doc, "r") 
 		ocurrencias = 0
 		fila = [0 for c in range (len(palabras))]
@@ -18,7 +17,6 @@ def matriz_ocurrencias (documentos, palabras) :
 				fila[cont] += linea.count(palabra)
 				cont +=1
 		matriz.append(fila)	
-		print (fila)
 	return matriz	
 	
 
@@ -27,7 +25,6 @@ def matriz_ocurrencias (documentos, palabras) :
 #~ arreglo del tamano del numero de palabras  
 #ni: arreglo del tamano del numero de palabras
 def matriz_similitud (matriz, N,n_palabras) :
-	print "\n"
 	maxs = [ ]
 	Ni = []
 	#~ maxs = matriz[:][0:] 
@@ -35,28 +32,33 @@ def matriz_similitud (matriz, N,n_palabras) :
 		columna =[matriz[pal][i] for pal in range(N)]
 		maxs.append(max(columna))
 		Ni.append (len ([i for i, e in enumerate(columna) if e != 0]))
-	print "maxs"
-	print maxs
-	print "\n"
-	print "Ni"
-	print Ni
-	print "\n"
 	for i in range (len (matriz)):
 		for j in range (len (matriz [i])):
 			try:
-				matriz [i] [j] = float ( float ((matriz [i] [j] /( float ((matriz [i] [j]) * maxs[j])))) * float (log (N/float (Ni[j]),10)))
-				print float (log (N/float (Ni[j]),10))
-				
+				matriz [i] [j] = float ( float ((matriz [i] [j] /( float ((maxs[j])))) * float (log (N/float (Ni[j]),10))))
 			except Exception:
 				 matriz [i] [j] = "0"
-	print matriz		 
-		
-#~ print (matriz)
+	return matriz			 
+				 
+def escribir(matriz, palabras):
+	f=open("matriz.arff","w")
+	f.write("@relation matriz\n")
+	for palabra in palabras:
+		f.write("@attribute "+palabra+" numeric\n")
+	f.write("@data\n")
+	for i in matriz:
+		for j in range(len(i)-1):
+			f.write(str(i[j])+", ")
+		f.write(str(i[len(i)-1]))
+		f.write("\n")
+	f.close()
+	
 directorio = "preprocesados/"
 palabras = ["colombi", "petr", "procur", "nacion", "bogot", "mineri", "dat", "clust", "clasificacion", "min", "mining", "futbol", "jugador", "mundial", "falca", "messi", "fif", "fifa", "cop"]	
 documentos = leer (directorio)
 matriz = matriz_ocurrencias (documentos,palabras)
-matriz_similitud (matriz,len(documentos),len(palabras))
+matriz_similitud = matriz_similitud (matriz,len(documentos),len(palabras))
+escribir(matriz_similitud, palabras)
 
 			 
 		
